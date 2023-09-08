29dc95fe4798d59e4f0f9a96dac2473d40f38ad8
Linaaaah <tpwls00490@gmail.com>
2023-09-08 16:30:02 +1000

    new codes on wk06

--- WK06.py
+++ WK06.py
@@ -1,106 +1,3 @@
-###########################################################################
-## Python code generated with wxFormBuilder (version 3.10.1-0-g8feb16b3)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO *NOT* EDIT THIS FILE!
-###########################################################################
-
-import wx
-import wx.xrc
-import wx.grid
-
-###########################################################################
-## Class Software
-###########################################################################
-
-class Software ( wx.Frame ):
-
-	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Airbnb Data Analysis Software", pos = wx.DefaultPosition, size = wx.Size( 1168,349 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
-
-		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
-		self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_INACTIVEBORDER ) )
-
-		bSizer3 = wx.BoxSizer( wx.VERTICAL )
-
-		bSizer4 = wx.BoxSizer( wx.VERTICAL )
-
-		gSizer4 = wx.GridSizer( 3, 10, 0, 0 )
-
-		gSizer4.SetMinSize( wx.Size( -3,-3 ) )
-		self.m_staticText12 = wx.StaticText( self, wx.ID_ANY, u"Suburb", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText12.Wrap( -1 )
-
-		gSizer4.Add( self.m_staticText12, 0, wx.ALL, 5 )
-
-		m_comboBox5Choices = []
-		self.m_comboBox5 = wx.ComboBox( self, wx.ID_ANY, u"Suburb", wx.DefaultPosition, wx.DefaultSize, m_comboBox5Choices, 0 )
-		gSizer4.Add( self.m_comboBox5, 0, 0, 5 )
-
-		self.m_staticText13 = wx.StaticText( self, wx.ID_ANY, u"Minimum Price", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText13.Wrap( -1 )
-
-		gSizer4.Add( self.m_staticText13, 0, wx.ALL, 5 )
-
-		self.m_textCtrl10 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer4.Add( self.m_textCtrl10, 0, wx.ALL, 5 )
-
-		self.m_staticText14 = wx.StaticText( self, wx.ID_ANY, u"Maximum Price", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText14.Wrap( -1 )
-
-		gSizer4.Add( self.m_staticText14, 0, wx.ALL, 5 )
-
-		self.m_textCtrl11 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer4.Add( self.m_textCtrl11, 0, wx.ALL, 5 )
-
-		self.m_staticText15 = wx.StaticText( self, wx.ID_ANY, u"Check-In Date", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText15.Wrap( -1 )
-
-		gSizer4.Add( self.m_staticText15, 0, wx.ALL, 5 )
-
-		m_comboBox6Choices = []
-		self.m_comboBox6 = wx.ComboBox( self, wx.ID_ANY, u"Date", wx.DefaultPosition, wx.DefaultSize, m_comboBox6Choices, 0 )
-		gSizer4.Add( self.m_comboBox6, 0, wx.ALL, 5 )
-
-		self.m_staticText16 = wx.StaticText( self, wx.ID_ANY, u"Check-Out Date", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText16.Wrap( -1 )
-
-		gSizer4.Add( self.m_staticText16, 0, wx.ALL, 5 )
-
-		m_comboBox7Choices = []
-		self.m_comboBox7 = wx.ComboBox( self, wx.ID_ANY, u"Date", wx.DefaultPosition, wx.DefaultSize, m_comboBox7Choices, 0 )
-		gSizer4.Add( self.m_comboBox7, 0, wx.ALL, 5 )
-
-		self.m_staticText19 = wx.StaticText( self, wx.ID_ANY, u"Room Type", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText19.Wrap( -1 )
-
-		self.m_staticText19.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_INACTIVEBORDER ) )
-
-		gSizer4.Add( self.m_staticText19, 0, wx.ALL, 5 )
-
-		m_comboBox8Choices = []
-		self.m_comboBox8 = wx.ComboBox( self, wx.ID_ANY, u"Room type", wx.DefaultPosition, wx.DefaultSize, m_comboBox8Choices, 0 )
-		gSizer4.Add( self.m_comboBox8, 0, wx.ALL, 5 )
-
-		self.m_staticText17 = wx.StaticText( self, wx.ID_ANY, u"Search Keyword", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText17.Wrap( -1 )
-
-		gSizer4.Add( self.m_staticText17, 0, wx.ALL, 5 )
-
-		self.m_textCtrl12 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer4.Add( self.m_textCtrl12, 0, wx.ALL, 5 )
-
-		self.m_staticText18 = wx.StaticText( self, wx.ID_ANY, u"Cleanliness Keyword", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText18.Wrap( -1 )
-
-		gSizer4.Add( self.m_staticText18, 0, wx.ALL, 5 )
-
-		self.m_textCtrl13 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-... (53줄 남음)
-접기
-message.txt
-6KB
-﻿
 # -*- coding: utf-8 -*-
 
 ###########################################################################
@@ -249,9 +146,19 @@ class Software ( wx.Frame ):
 
 		self.Centre( wx.BOTH )
 
+		# Connect Events
+		self.Search.Bind( wx.EVT_BUTTON, self.OnSearch )
+		self.m_button2.Bind( wx.EVT_BUTTON, self.OnPlot )
+
 	def __del__( self ):
 		pass
 
 
-message.txt
-6KB
+	# Virtual event handlers, override them in your derived class
+	def OnSearch( self, event ):
+		event.Skip()
+
+	def OnPlot( self, event ):
+		event.Skip()
+
+
---
+++ function.py
@@ -0,0 +1,26 @@
+import wx
+import wx.grid
+import pandas as pd
+
+from WK06 import Software as Software2
+
+
+
+
+
+def OnSearch(self, event):
+    min_price = float(self.m_textCtrl0.GetValue())
+    ## change the temperature to price
+    # df_1 = df[df['temperature_celsius'] > min_price]
+
+    max_price = float(self.m_textCtrl11.GetValue())
+    ## change the temperature to price
+    # df_1 = df_1[df_1['temperature_celsius'] < max_price]
+
+    tabel = DataTable(df_1)
+    self.m_grid2.ClearGrid()
+    self.m_grid2.SetTable(tabel, True)
+    self.m_grid2.AutoSize()
+    self.Layout()
+
+
