d0a3a6b9d2634e5931e5dcf39b29b013dd1d1b96
Sarah Mitchell <itsrara04@gmail.com>
2023-09-26 08:56:48 +1000

    updated keyword and cleanliness

--- Cleanliness_Run.py
+++ Cleanliness_Run.py
@@ -2,79 +2,123 @@ import wx
 import wx.grid
 import pandas as pd
 
-class Cleanliness ( wx.Frame ):
+
+class Cleanliness (wx.Frame):
 
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Airbnb Data Software",
-							pos = wx.DefaultPosition, size = wx.Size( 874,300 ),
-							style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__(self, parent, id = wx.ID_ANY, title = u"Airbnb Data Software", pos = wx.DefaultPosition, size = wx.Size(874, 300), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
-		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
-		self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_INACTIVEBORDER ) )
+		self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
+		self.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_INACTIVEBORDER))
 
-		bSizer1 = wx.BoxSizer( wx.VERTICAL )
+		bSizer1 = wx.BoxSizer(wx.VERTICAL)
 
-		self.m_panel2 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-		gSizer3 = wx.GridSizer( 1, 3, 0, 0 )
+		self.m_panel2 = wx.Panel(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+		fgSizer11 = wx.FlexGridSizer(2, 3, 0, 0)
+		fgSizer11.SetFlexibleDirection(wx.BOTH)
+		fgSizer11.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
-		self.m_staticText4 = wx.StaticText( self.m_panel2, wx.ID_ANY, u"Type Cleanliness Keyword", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText4.Wrap( -1 )
+		self.m_staticText4 = wx.StaticText(self.m_panel2, wx.ID_ANY, u"Select Cleanliness Keyword", wx.DefaultPosition, wx.DefaultSize, 0)
+		self.m_staticText4.Wrap(-1)
 
-		gSizer3.Add( self.m_staticText4, 0, wx.ALL, 5 )
+		fgSizer11.Add(self.m_staticText4, 0, wx.ALL, 5)
 
-		self.m_textCtrl2 = wx.TextCtrl( self.m_panel2, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer3.Add( self.m_textCtrl2, 0, wx.ALL, 5 )
+		m_comboBox1Choices = ['Aseptic', 'Crisp', 'Disinfected', 'Elegant', 'Fresh', 'Gleaming', 'Hygienic',
+							  'Immaculate', 'Neat', 'Pristine', 'Sanitary', 'Shining', 'Spotless', 'Sterile',
+							  'Tidy', 'Ultraclean', 'Unblemished', 'Well-kept']
 
-		self.m_button6 = wx.Button( self.m_panel2, wx.ID_ANY, u"Search", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_button6.SetBackgroundColour( wx.Colour( 255, 255, 174 ) )
+		self.m_comboBox1 = wx.ComboBox(self.m_panel2, wx.ID_ANY, u"Select", wx.DefaultPosition, wx.DefaultSize, m_comboBox1Choices, 0)
+		fgSizer11.Add(self.m_comboBox1, 0, wx.ALL, 5)
 
-		gSizer3.Add( self.m_button6, 0, wx.ALL, 5 )
+		self.m_button6 = wx.Button(self.m_panel2, wx.ID_ANY, u"Search", wx.DefaultPosition, wx.DefaultSize, 0)
+		self.m_button6.SetBackgroundColour(wx.Colour(255, 255, 174))
 
+		fgSizer11.Add(self.m_button6, 0, wx.ALL, 5)
 
-		self.m_panel2.SetSizer( gSizer3 )
+		self.result_count_label = wx.StaticText(self.m_panel2, wx.ID_ANY, "", wx.DefaultPosition, wx.DefaultSize, 0)
+		fgSizer11.Add(self.result_count_label, 0, wx.ALL, 5)
+
+		self.m_panel2.SetSizer(fgSizer11)
 		self.m_panel2.Layout()
-		gSizer3.Fit( self.m_panel2 )
-		bSizer1.Add( self.m_panel2, 1, wx.EXPAND |wx.ALL, 5 )
+		fgSizer11.Fit(self.m_panel2)
+		bSizer1.Add(self.m_panel2, 1, wx.EXPAND | wx.ALL, 5)
 
-		self.m_grid2 = wx.grid.Grid( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
+		fgSizer12 = wx.FlexGridSizer(0, 2, 0, 0)
+		fgSizer12.SetFlexibleDirection(wx.BOTH)
+		fgSizer12.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
+
+		self.m_grid2 = wx.grid.Grid(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0)
 
 		# Grid
-		self.m_grid2.CreateGrid( 5, 5 )
-		self.m_grid2.EnableEditing( True )
-		self.m_grid2.EnableGridLines( True )
-		self.m_grid2.EnableDragGridSize( False )
-		self.m_grid2.SetMargins( 0, 0 )
+		self.m_grid2.CreateGrid(5, 2)
+		self.m_grid2.EnableEditing(True)
+		self.m_grid2.EnableGridLines(True)
+		self.m_grid2.EnableDragGridSize(False)
+		self.m_grid2.SetMargins(0, 0)
 
 		# Columns
-		self.m_grid2.EnableDragColMove( False )
-		self.m_grid2.EnableDragColSize( True )
-		self.m_grid2.SetColLabelAlignment( wx.ALIGN_CENTER, wx.ALIGN_CENTER )
+		self.m_grid2.EnableDragColMove(False)
+		self.m_grid2.EnableDragColSize(True)
+		self.m_grid2.SetColLabelAlignment(wx.ALIGN_LEFT, wx.ALIGN_CENTER)
 
 		# Rows
-		self.m_grid2.EnableDragRowSize( True )
-		self.m_grid2.SetRowLabelAlignment( wx.ALIGN_CENTER, wx.ALIGN_CENTER )
+		self.m_grid2.EnableDragRowSize(True)
+		self.m_grid2.SetRowLabelAlignment(wx.ALIGN_CENTER, wx.ALIGN_CENTER)
 
 		# Label Appearance
 
 		# Cell Defaults
-		self.m_grid2.SetDefaultCellAlignment( wx.ALIGN_LEFT, wx.ALIGN_TOP )
-		bSizer1.Add( self.m_grid2, 0, wx.ALL, 5 )
+		self.m_grid2.SetDefaultCellAlignment(wx.ALIGN_LEFT, wx.ALIGN_TOP)
+		fgSizer12.Add(self.m_grid2, 0, wx.ALL, 5)
 
+		bSizer1.Add(fgSizer12, 1, wx.EXPAND, 5)
 
-		self.SetSizer( bSizer1 )
+		self.SetSizer(bSizer1)
 		self.Layout()
 
-		self.Centre( wx.BOTH )
+		self.Centre(wx.BOTH)
 
 		# Connect Events
-		self.m_button6.Bind( wx.EVT_BUTTON, self.OnSearch )
+		self.m_button6.Bind(wx.EVT_BUTTON, self.OnSearch)
+
+		# Column names
+		self.m_grid2.SetColLabelValue(0, 'Listing ID')
+		self.m_grid2.SetColLabelValue(1, 'Comments')
 
 	def __del__( self ):
 		pass
 
+	def OnSearch(self, event):
+		try:
+			cleanliness_keyword = self.m_comboBox1.GetValue()
 
-	# Virtual event handlers, override them in your derived class
-	def OnSearch( self, event ):
-		event.Skip()
+			df_reviews = pd.read_csv("reviews_dec18.csv", usecols=['listing_id', 'comments'])
+			df_filtered_reviews = df_reviews[df_reviews['comments'].str.contains(cleanliness_keyword, case=False, na=False)]
 
+			num_rows = len(df_filtered_reviews)
+			self.result_count_label.SetLabel(f"Number of Results: {num_rows}")
+
+			self.m_grid2.ClearGrid()
+			self.m_grid2.AppendRows(num_rows)
+
+			self.m_grid2.AppendCols(len(df_filtered_reviews.columns))
+			self.m_grid2.SetColLabelValue(0, 'Listing ID')
+			self.m_grid2.SetColLabelValue(1, 'Comments')
+
+			for row, (_, row_data) in enumerate(df_filtered_reviews.iterrows()):
+				for col, value in enumerate(row_data):
+					self.m_grid2.SetCellValue(row, col, str(value))
+
+			self.m_grid2.AutoSize()
+			self.m_grid2.Refresh()
+
+		except Exception as e:
+			wx.LogError(f"Error: {e}")
+			return
+
+if __name__ == "__main__":
+	app = wx.App(False)
+	frame = Cleanliness(None)
+	frame.Show(True)
+	app.MainLoop()
 
--- Keyword_Run.py
+++ Keyword_Run.py
@@ -15,7 +15,9 @@ class Keyword(wx.Frame):
         bSizer1 = wx.BoxSizer(wx.VERTICAL)
 
         self.m_panel2 = wx.Panel(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
-        gSizer3 = wx.GridSizer(2, 4, 0, 0)
+        fgSizer9 = wx.FlexGridSizer(3, 4, 0, 0)
+        fgSizer9.SetFlexibleDirection(wx.BOTH)
+        fgSizer9.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
         # Read the unique dates from the CSV file
         df_calendar = pd.read_csv("calendar_dec18.csv")
@@ -25,42 +27,48 @@ class Keyword(wx.Frame):
         self.m_staticText2 = wx.StaticText(self.m_panel2, wx.ID_ANY, u"Check-In Date", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_staticText2.Wrap(-1)
 
-        gSizer3.Add(self.m_staticText2, 0, wx.ALL, 5)
+        fgSizer9.Add(self.m_staticText2, 0, wx.ALL, 5)
 
         self.m_comboBox2 = wx.ComboBox(self.m_panel2, wx.ID_ANY, u"Select Date", wx.DefaultPosition, wx.DefaultSize, unique_dates, 0)
-        gSizer3.Add(self.m_comboBox2, 0, wx.ALL, 5)
+        fgSizer9.Add(self.m_comboBox2, 0, wx.ALL, 5)
 
         self.m_staticText3 = wx.StaticText(self.m_panel2, wx.ID_ANY, u"Check-Out Date", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_staticText3.Wrap(-1)
 
-        gSizer3.Add(self.m_staticText3, 0, wx.ALL, 5)
+        fgSizer9.Add(self.m_staticText3, 0, wx.ALL, 5)
 
         self.m_comboBox3 = wx.ComboBox(self.m_panel2, wx.ID_ANY, u"Select Date", wx.DefaultPosition, wx.DefaultSize, unique_dates, 0)
-        gSizer3.Add(self.m_comboBox3, 0, wx.ALL, 5)
+        fgSizer9.Add(self.m_comboBox3, 0, wx.ALL, 5)
 
-        # Add a text input field for the keyword
         self.m_staticText4 = wx.StaticText(self.m_panel2, wx.ID_ANY, u"Keyword", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_staticText4.Wrap(-1)
 
-        gSizer3.Add(self.m_staticText4, 0, wx.ALL, 5)
+        fgSizer9.Add(self.m_staticText4, 0, wx.ALL, 5)
 
-        self.m_textCtrl1 = wx.TextCtrl(self.m_panel2, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        gSizer3.Add(self.m_textCtrl1, 0, wx.ALL, 5)
+        self.m_textCtrl2 = wx.TextCtrl(self.m_panel2, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+        fgSizer9.Add(self.m_textCtrl2, 0, wx.ALL, 5)
 
         self.m_button8 = wx.Button(self.m_panel2, wx.ID_ANY, u"Search", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_button8.SetBackgroundColour(wx.Colour(255, 255, 174))
 
-        gSizer3.Add(self.m_button8, 0, wx.ALL, 5)
+        fgSizer9.Add(self.m_button8, 0, wx.ALL, 5)
 
-        self.m_panel2.SetSizer(gSizer3)
+        self.m_panel2.SetSizer(fgSizer9)
         self.m_panel2.Layout()
-        gSizer3.Fit(self.m_panel2)
+        fgSizer9.Fit(self.m_panel2)
         bSizer1.Add(self.m_panel2, 1, wx.EXPAND | wx.ALL, 5)
+
+        self.result_count_label = wx.StaticText(self.m_panel2, wx.ID_ANY, "", wx.DefaultPosition, wx.DefaultSize, 0)
+        fgSizer9.Add(self.result_count_label, 0, wx.ALL, 5)
+
+        fgSizer10 = wx.FlexGridSizer(0, 2, 0, 0)
+        fgSizer10.SetFlexibleDirection(wx.BOTH)
+        fgSizer10.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
         self.m_grid2 = wx.grid.Grid(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0)
 
         # Grid
-        self.m_grid2.CreateGrid(5, 5)
+        self.m_grid2.CreateGrid(5, 3)
         self.m_grid2.EnableEditing(True)
         self.m_grid2.EnableGridLines(True)
         self.m_grid2.EnableDragGridSize(False)
@@ -89,6 +97,12 @@ class Keyword(wx.Frame):
         # Connect Events
         self.m_button8.Bind(wx.EVT_BUTTON, self.OnSearch)
 
+        # Column names
+        self.m_grid2.SetColLabelValue(0, 'Listing ID')
+        self.m_grid2.SetColLabelValue(1, 'Name')
+        self.m_grid2.SetColLabelValue(2, 'Description')
+
+
     def __del__(self):
         pass
 
@@ -97,7 +111,7 @@ class Keyword(wx.Frame):
             checkin_date = self.m_comboBox2.GetValue()
             checkout_date = self.m_comboBox3.GetValue()
 
-            keyword = self.m_textCtrl1.GetValue()  # Get the keyword entered by the user
+            keyword = self.m_textCtrl2.GetValue()  # Get the keyword entered by the user
 
             if not checkin_date or not checkout_date:
                 wx.MessageBox("Please select both Check-In and Check-Out dates.", "Error", wx.OK | wx.ICON_ERROR)
@@ -108,25 +122,28 @@ class Keyword(wx.Frame):
             checkout_date = pd.Timestamp(checkout_date)
 
             # Read the data
-            df_calendar = pd.read_csv("calendar_dec18.csv", engine='python')
+            df_calendar = pd.read_csv("calendar_dec18.csv", usecols=['listing_id', 'date'])
             df_calendar['date'] = pd.to_datetime(df_calendar['date'])
 
-            df_listings = pd.read_csv("listings_dec18.csv", low_memory=False, usecols=['id', 'name', 'description'])
+            df_calendar_filtered = df_calendar[(df_calendar['date'] >= checkin_date) & (df_calendar['date'] <= checkout_date)]
 
-            df_filtered = df_calendar[(df_calendar['date'] >= checkin_date) & (df_calendar['date'] <= checkout_date)]
+            df_listings = pd.read_csv("listings_dec18.csv", usecols=['id', 'name', 'description'])
 
-            df_filtered_listings = df_listings[df_listings['description'].str.contains(keyword, case=False, na=False)]
+            df_listings_filtered = df_listings[df_listings['description'].str.contains(keyword, case=False, na=False)]
 
-            num_rows = (len(df_filtered_listings) - 5)
+            num_rows = len(df_listings_filtered)
+            self.result_count_label.SetLabel(f"Number of Results: {num_rows}")
+
+            self.m_grid2.ClearGrid()
             self.m_grid2.AppendRows(num_rows)
 
-            self.m_grid2.AppendCols(len(df_filtered_listings.columns))
+            self.m_grid2.AppendCols(len(df_listings_filtered.columns))
             self.m_grid2.SetColLabelValue(0, 'ID')
             self.m_grid2.SetColLabelValue(1, 'Name')
             self.m_grid2.SetColLabelValue(2, 'Description')
 
             # Fill in the data in the grid
-            for row, (_, row_data) in enumerate(df_filtered_listings.iterrows()):
+            for row, (_, row_data) in enumerate(df_listings_filtered.iterrows()):
                 for col, value in enumerate(row_data):
                     self.m_grid2.SetCellValue(row, col, str(value))
 
@@ -138,8 +155,9 @@ class Keyword(wx.Frame):
             wx.LogError(f"Error: {e}")
             return
 
-        if df_filtered.empty or df_filtered_listings.empty:
-            wx.MessageBox(f"No data available for the selected date range with keyword '{keyword}'.", "Info", wx.OK | wx.ICON_INFORMATION)
+        if df_calendar_filtered.empty or df_listings_filtered.empty:
+            wx.MessageBox(f"No data available for the selected date range with keyword '{keyword}'.", "Info",
+                          wx.OK | wx.ICON_INFORMATION)
             return
 
 if __name__ == "__main__":
